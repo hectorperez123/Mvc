14da37ab09e5c5bfa41fd85d7c1a6b1a4bc5c42d
SuperUs <SuperUs@DESKTOP-87IKLV8>
2020-04-07 15:20:09 -0500

    Se crea el proyecto y trabajamos en el modelo

---
+++ ProyectoMVC/src/ProyectoMVC.java
@@ -0,0 +1,16 @@
+
+
+/**
+ *
+ * @author SuperUs
+ */
+public class ProyectoMVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
---
+++ ProyectoMVC/src/modelo/MenuDia.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author SuperUs
+ */
+public class MenuDia {
+   private String plato;
+   private String adiciones;
+   private int cantidad;
+
+    public MenuDia(String plato, String adiciones, int cantidad) {
+        this.plato = plato;
+        this.adiciones = adiciones;
+        this.cantidad = cantidad;
+    }
+
+    public String getPlato() {
+        return plato;
+    }
+
+    public void setPlato(String plato) {
+        this.plato = plato;
+    }
+
+    public String getAdiciones() {
+        return adiciones;
+    }
+
+    public void setAdiciones(String adiciones) {
+        this.adiciones = adiciones;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+   
+}
